7438b8dac151548c1d1433625fdfb52198816033
Hary_Nguyen (King) <namlocan111@gmail.com>
2024-10-20 13:34:51 +0700

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,46 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent = `On women’s day what can I wish for, but the very best for you! Happy Women’s Day ======<3====== "Vào ngày Quốc tế phụ nữ, tớ mong ước mọi thứ tốt đẹp nhất sẽ đến với bạn. Chúc bạn một 20/10 hạnh phúc!!!                  GOD BLESS YOU!! " `                  
+
+                   
+
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 100
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
